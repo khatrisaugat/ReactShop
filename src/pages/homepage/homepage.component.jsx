ce2e340e3831396422b787aed862a99461538004
khatrisaugat <khatri.saugat55@gmail.com>
2022-08-22 11:12:15 +0545

    smaller components and menu-items styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./homepage.style.css";
+import DirectoryMenu from "../../components/directory/directory.component";
+
+function Homepage() {
+  return (
+    <div className="homepage">
+      <DirectoryMenu />
+    </div>
+  );
+}
+
+export default Homepage;

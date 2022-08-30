7bf79590425122d1e746f1055ee527a7fdc2719e
khatrisaugat <khatri.saugat55@gmail.com>
2022-08-30 22:00:13 +0545

    cart-items showing up in the cart dropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import "./cart-dropdown.style.css";
 import CustomButton from "../custom-button/custom-button.component";
 import { connect } from "react-redux";
-import { toggleCartHidden } from "./../../redux/cart/cart.actions";
+import CartItem from "./../cart-item/cart-item.component";
 
-function CartDropdown({ toggleCartHidden }) {
+function CartDropdown({ items }) {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
-        <span className="empty-message">Your cart is empty</span>
+        {items.map((item) => (
+          <CartItem item={item} />
+        ))}
+
+        {items.length === 0 && (
+          <span className="empty-message">Your cart is empty</span>
+        )}
       </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 }
-const mapDispatchToProps = (Dispatch) => ({
-  toggleCartHidden: () => Dispatch(toggleCartHidden()),
+const mapStateToProps = ({ cart: { items } }) => ({
+  items,
 });
-
-export default connect(null, mapDispatchToProps)(CartDropdown);
+export default connect(mapStateToProps)(CartDropdown);
